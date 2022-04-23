679262043dd2d4dc7b8e6bb1792fab20a154b50d
krnlk <95872735+krnlk@users.noreply.github.com>
2022-04-23 15:57:17 +0200

    Starting to work on login.

--- ripple-hot-seat/src/main/java/com/comarch/ripplehotseat/RippleHotSeatApplication.java
+++ ripple-hot-seat/src/main/java/com/comarch/ripplehotseat/RippleHotSeatApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import com.comarch.ripplehotseat.model.Room;
 import com.comarch.ripplehotseat.model.User;
@@ -16,6 +17,7 @@ import com.comarch.ripplehotseat.service.RoomService;
 import com.comarch.ripplehotseat.service.UserService;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class RippleHotSeatApplication implements CommandLineRunner {
 
 	@Autowired
--- ripple-hot-seat/src/main/java/com/comarch/ripplehotseat/controller/UserRestController.java
+++ ripple-hot-seat/src/main/java/com/comarch/ripplehotseat/controller/UserRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.comarch.ripplehotseat.dto.UserDTO;
@@ -45,6 +46,16 @@ public class UserRestController {
 	public UserDTO findByLogin(@PathVariable("login") String login) {
 		return ObjectMapperUtils.map(userService.findByLogin(login), UserDTO.class);
 	}
+	
+	@GetMapping(value = "/login")
+    public ResponseEntity<?> login(@RequestParam("login") String login, @RequestParam("password") String password) {
+        return new ResponseEntity<String>("ok", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/logout")
+    public ResponseEntity<?> logout() {
+        return new ResponseEntity<String>("ok", HttpStatus.OK);
+    }
 	
 	@PostMapping(value = "/save")
 	public ResponseEntity<?> save(@RequestBody UserDTO userDTO) {
